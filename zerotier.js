d5a001c7c94bf4f02bbd9699d515b6d8db13b933
root <root@ultra.localdomain>
2020-06-30 02:35:51 -0400

    if member.name is blank use nodeId

--- zerotier.js
+++ zerotier.js
@@ -7,11 +7,16 @@ module.exports.getZTMembers = function getZTMembers() {
             url: `/api/network/${process.env.zt_network}/member`,
             headers: { 'Authorization': "bearer " + process.env.zt_api_key }
         }
+        
         var ztData = await request(ztOptions)
         var memberList = []
         for (member of ztData.data) {
             var addme = {}
-            addme.name = member.name
+            if (member.name != "") {
+                addme.name = member.name
+            } else {
+                addme.name = member.nodeId
+            }
             addme.ip = member.config.ipAssignments[0]
             memberList.push(addme)
         }
